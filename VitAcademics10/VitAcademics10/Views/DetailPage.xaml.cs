745c5fea300bdbffbd996d5615f08854668d9f73
Shubham Jain <bitloks@gmail.com>
2017-03-11 00:09:40 +0530

    Initialize

---
+++ VitAcademics10/VitAcademics10/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using VitAcademics10.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace VitAcademics10.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
