e824b3df552d637537d873e899cd330e064eba01
Shubham Jain <bitloks@gmail.com>
2017-03-12 03:46:42 +0530

    User Data Model Ready

---
+++ VitAcademics10/AcademicsLibrary/DataModel/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicsLibrary.DataModel
+{
+    public class User
+    {
+        
+        public string RegNo{get;private set;}
+        public string Pass   { get; private set; }
+        public string Campus { get; private set; }
+
+        public User(string campus, string reg, string pass)
+        {
+            Campus = campus;
+            RegNo = reg;
+            Pass = pass;
+
+        }
+   
+
+    }
+}
