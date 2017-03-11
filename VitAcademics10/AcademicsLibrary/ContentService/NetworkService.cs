b32a766cab8f9dec6156c666bbcba8708303164a
Shubham Jain <bitloks@gmail.com>
2017-03-12 03:46:29 +0530

    Basic Login and Refresh feature added

---
+++ VitAcademics10/AcademicsLibrary/ContentService/NetworkService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcademicsLibrary.DataModel;
+
+namespace AcademicsLibrary.NetworkService
+{
+    public class NetworkService
+    {
+        private const string BASE_URI_STRING = "http://myffcs.in:8080/campus/";
+
+        private const string LOGIN_STRING_FORMAT = "{0}/login/";
+        private const string REFRESH_STRING_FORMAT = "{0}/refresh/";
+        private const string COURSEPAGE_STRING_FORMAT = "{0}/coursepage/data/";
+        private static Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+        private static Windows.Web.Http.Headers.HttpRequestHeaderCollection headers = httpClient.DefaultRequestHeaders;
+        private static Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+
+
+        public static async Task<string> Login(string campus, string reg, string pass)
+        {
+            string httpResponseBody = "";
+            var user = new User(campus, reg, pass);
+            try
+            {
+                var postContent = new Windows.Web.Http.HttpFormUrlEncodedContent(
+                                    new KeyValuePair<string, string>[2] {
+                                        new KeyValuePair<string, string>("regNo",user.RegNo),
+                                        new KeyValuePair<string, string>("psswd",user.Pass),
+
+                                    });
+                string uriString = BASE_URI_STRING + String.Format(LOGIN_STRING_FORMAT, campus);
+                httpResponse = await httpClient.PostAsync(new Uri(uriString), postContent);
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+                return httpResponseBody;
+            }
+
+            catch (Exception e)
+            {
+                httpResponseBody = "Error: " + e.HResult.ToString("X") + " Message: " + e.Message;
+                return httpResponseBody;
+            }
+        }
+        public static async Task<string> Refresh(string campus,string reg,string pass)
+        {
+            string httpResponseBody = "";
+            var user = new User(campus,reg,pass);  
+            try
+            {
+                var postContent = new Windows.Web.Http.HttpFormUrlEncodedContent(
+                                    new KeyValuePair<string, string>[2] {
+                                        new KeyValuePair<string, string>("regNo",user.RegNo),
+                                        new KeyValuePair<string, string>("psswd",user.Pass),
+
+                                    });
+                string uriString = BASE_URI_STRING + String.Format(REFRESH_STRING_FORMAT, campus);
+                httpResponse = await httpClient.PostAsync(new Uri(uriString), postContent);
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+                return httpResponseBody;
+            }
+
+            catch (Exception e)
+            {
+                httpResponseBody = "Error: " + e.HResult.ToString("X") + " Message: " + e.Message;
+                return httpResponseBody;
+            }
+        }
+
+    }
+}
