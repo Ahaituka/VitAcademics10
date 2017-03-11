c2ca751342672f45a6e638f2b74c84be8a7b8119
Shubham Jain <bitloks@gmail.com>
2017-03-12 03:46:49 +0530

    Helpers added

---
+++ VitAcademics10/AcademicsLibrary/Helpers/MessageDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace AcademicsLibrary.Helpers
+{
+    public class MessageDialog
+    {
+        public async static void ShowDialog(string message)
+        {
+            var dlg = new Windows.UI.Popups.MessageDialog(message);
+            await dlg.ShowAsync();
+        }
+
+    }
+}
